b848053adbcca1f66275b2708b5c29304d49ffd6
Nipun Sampath <nipunsampath@gmail.com>
2020-08-11 11:06:23 +0530

    removed users.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const users = require("./users");
 const dotenv = require("dotenv").config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
